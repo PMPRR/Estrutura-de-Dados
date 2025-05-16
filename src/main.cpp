7c608b0515b6aeb1c11e9f089685b6a7947a3bf1
Paulo Ramalho <paulodemelo2006@gmail.com>
2025-05-16 01:27:12 -0300

    Update docker.dev

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,6 @@ int main() {
     std::cout << "Hello World" << std::endl;
     int a = test();
     std::cout << a << std::endl;
+    std::cout << "I changed the docker" << std::endl;
     return 0;
 }
